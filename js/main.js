d6c77bb2b88fba8ad36bcf959235e32a6d418f25
voidrak <112881704+voidrak@users.noreply.github.com>
2023-08-07 20:48:35 +0300

    some minor improvement

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const header = document.querySelector(".header");
+const submitEL = document.getElementById("submit-el");
 
 header.addEventListener("click", () => {
   header.classList.toggle("clicked");
+});
+
+submitEL.addEventListener("click", function () {
+  window.alert("You Have Successfully Submitted");
 });
